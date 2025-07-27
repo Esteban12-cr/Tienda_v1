fd16372fe28a526a888ea73cf771a525e9ef89d9
Angie470 <aborbon70131@ufide.ac.cr>
2025-07-27 15:55:45 -0600

    Tarea 4
    finalizacion de  la parte de seguridad del proyecto

--- src/main/java/com/tienda/TiendaApplication.java
+++ src/main/java/com/tienda/TiendaApplication.java
@@ -1,13 +1,13 @@
 package com.tienda;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class TiendaApplicationTests {
+@SpringBootApplication
+public class TiendaApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaApplication.class, args);
+    }
 
 }
